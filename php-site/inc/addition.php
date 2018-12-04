1fa58eed58ceb695a3f830d0da6b657c8d2d5439
Joshua Duncan <41347949+JEduncan@users.noreply.github.com>
2018-12-03 19:00:46 -0500

    Add files via upload

---
+++ php-site/inc/addition.php
@@ -0,0 +1,30 @@
+<?php
+$addition = 5 + 5;
+echo $addition;
+echo "\n";
+echo "<br>";
+                // Result:10
+$subtraction = 5 - 5;
+echo $subtraction;
+echo "\n";
+echo "<br>";
+                // Result:0
+$multiplication = 5 * 5;
+echo $multiplication;
+echo "\n";
+echo "<br>";
+                // Result: 25
+$divison = 5 / 5;
+echo $divison;
+echo "\n";
+echo "<br>";
+                // Result:1
+
+$modulus = 7 % 5;
+                 // Result:2
+echo $multiplication;
+echo "\n";
+echo "<br>";
+echo 3 + 5 / 2;
+              // Result:5.5
+ ?>
---
+++ php-site/inc/variable.php
@@ -0,0 +1,8 @@
+<?php 
+$variableNamr = "I am a Boss.";
+$empty_variable = "";
+$eyes = "brown";
+$hair = 'black';
+$age = 21;
+
+ ?>
